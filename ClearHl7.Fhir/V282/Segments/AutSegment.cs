10ca51f4cf952bd12d6457903085e79a602a91df
Dave Bronson <dave.bronson@geyco.com>
2020-08-25 17:29:05 -0400

    Stubbed in V282 Segments.

---
+++ ClearHl7.Fhir/V282/Segments/AutSegment.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace ClearHl7.Fhir.V282.Segments
+{
+    /// <summary>
+    /// HL7 Version 2 Segment AUT - Authorization Information.
+    /// </summary>
+    public class AutSegment : ISegment
+    {
+        /// <summary>
+        /// Gets the ID for the Segment.  This property is read-only.
+        /// </summary>
+        public string Id { get; } = "AUT";
+        
+        /// <summary>
+        /// Gets or sets the rank, or ordinal, which describes the place that this Segment resides in an ordered list of Segments.
+        /// </summary>
+        public int Ordinal { get; set; }
+        
+        /// <summary>
+        /// Returns a delimited string representation of this instance.
+        /// </summary>
+        /// <returns>A string.</returns>
+        public string ToDelimitedString()
+        {
+            System.Globalization.CultureInfo culture = System.Globalization.CultureInfo.CurrentCulture;
+
+            return string.Format(
+                                culture,
+                                "{0}|{1}|{2}|{3}|{4}|{5}|{6}|{7}|{8}|{9}|{10}|{11}|{12}|{13}|{14}|{15}|{16}|{17}|{18}|{19}|{20}|{21}|{22}|{23}|{24}|{25}|{26}|{27}|{28}|{29}|{30}|{31}|{32}|{33}|{34}|{35}|{36}|{37}|{38}|{39}"
+                                ).TrimEnd('|');
+        }
+    }
+}
---
+++ ClearHl7.Fhir/V282/Segments/ISegment.cs
@@ -0,0 +1,10 @@
+﻿namespace ClearHl7.Fhir.V282.Segments
+{
+    public interface ISegment
+    {
+        string Id { get; }
+        int Ordinal { get; set; }
+
+        string ToDelimitedString();
+    }
+}
