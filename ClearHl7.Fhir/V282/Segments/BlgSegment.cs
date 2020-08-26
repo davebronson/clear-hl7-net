d75c9a0abbcb030077163756b8b1ba1b95f15232
Dave Bronson <dave.bronson@geyco.com>
2020-08-26 17:03:31 -0400

    Added fields to Segment classes.

--- ClearHl7.Fhir/V282/Segments/BlgSegment.cs
+++ ClearHl7.Fhir/V282/Segments/BlgSegment.cs
@@ -1,4 +1,5 @@
 using System;
+using ClearHl7.Fhir.V282.Types;
 
 namespace ClearHl7.Fhir.V282.Segments
 {
@@ -16,7 +17,30 @@ namespace ClearHl7.Fhir.V282.Segments
         /// Gets or sets the rank, or ordinal, which describes the place that this Segment resides in an ordered list of Segments.
         /// </summary>
         public int Ordinal { get; set; }
-        
+
+        /// <summary>
+        /// BLG.1 - When to Charge.
+        /// </summary>
+        /// <remarks>https://www.hl7.org/fhir/v2/0100</remarks>
+        public ChargeCodeAndDate WhenToCharge { get; set; }
+
+        /// <summary>
+        /// BLG.2 - Charge Type.
+        /// </summary>
+        /// <remarks>https://www.hl7.org/fhir/v2/0122</remarks>
+        public string ChargeType { get; set; }
+
+        /// <summary>
+        /// BLG.3 - Account ID.
+        /// </summary>
+        public ExtendedCompositeIdWithCheckDigit AccountId { get; set; }
+
+        /// <summary>
+        /// BLG.4 - Charge Type Reason.
+        /// </summary>
+        /// <remarks>https://www.hl7.org/fhir/v2/0475</remarks>
+        public CodedWithExceptions ChargeTypeReason { get; set; }
+
         /// <summary>
         /// Returns a delimited string representation of this instance.
         /// </summary>
@@ -27,7 +51,12 @@ namespace ClearHl7.Fhir.V282.Segments
 
             return string.Format(
                                 culture,
-                                "{0}|{1}|{2}|{3}|{4}|{5}|{6}|{7}|{8}|{9}|{10}|{11}|{12}|{13}|{14}|{15}|{16}|{17}|{18}|{19}|{20}|{21}|{22}|{23}|{24}|{25}|{26}|{27}|{28}|{29}|{30}|{31}|{32}|{33}|{34}|{35}|{36}|{37}|{38}|{39}"
+                                "{0}|{1}|{2}|{3}|{4}",
+                                Id,
+                                WhenToCharge?.ToDelimitedString(),
+                                ChargeType,
+                                AccountId?.ToDelimitedString(),
+                                ChargeTypeReason?.ToDelimitedString()
                                 ).TrimEnd('|');
         }
     }
