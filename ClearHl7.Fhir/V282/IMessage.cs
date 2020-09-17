a8236e51ab3de391efe704c395ae93f4e5472ffa
davebronson <38844647+davebronson@users.noreply.github.com>
2020-09-17 17:43:14 -0400

    Added Message class.

---
+++ ClearHl7.Fhir/V282/IMessage.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using ClearHl7.Fhir.V282.Segments;
+
+namespace ClearHl7.Fhir.V282
+{
+    public interface IMessage
+    {
+        IEnumerable<ISegment> Segments { get; set; }
+
+        string ToDelimitedString();
+    }
+}
---
+++ ClearHl7.Fhir/V282/Message.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ClearHl7.Fhir.V282.Segments;
+
+namespace ClearHl7.Fhir.V282
+{
+    public class Message : IMessage
+    {
+        /// <summary>
+        /// Gets or sets the list of Message Segments.
+        /// </summary>
+        public IEnumerable<ISegment> Segments { get; set; }
+
+        /// <summary>
+        /// Returns a delimited string representation of this instance.
+        /// </summary>
+        /// <returns>A string.</returns>
+        public string ToDelimitedString()
+        {
+            StringBuilder output = new StringBuilder();
+
+            if (Segments?.Any() == true)
+            {
+                foreach (ISegment item in Segments.OrderBy(x => x.Ordinal))
+                {
+                    output.Append(item.ToDelimitedString());
+                    output.Append((char)13);
+                }
+            }
+
+            return output.ToString();
+        }
+    }
+}
