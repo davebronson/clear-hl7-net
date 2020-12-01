114fbb60a247a26c0a847689e04fe9b3cba03e40
davebronson <dave.bronson@gmail.com>
2020-12-01 13:55:08 -0500

    Restructured Solution/Project folder hierarchy.

---
+++ ClearHl7.Fhir.Tests/SegmentsTests/AbsSegmentTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace ClearHl7.Fhir.Tests.SegmentsTests
+{
+    public class AbsSegmentTests
+    {
+        //[Fact]
+        //public void Test1()
+        //{
+
+        //}
+    }
+}
