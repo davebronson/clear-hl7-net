376df49d91a47e31958828fc5e3b18fb33717d7b
davebronson <dave.bronson@gmail.com>
2020-12-05 17:01:09 -0500

    Moved projects to src and test subfolders.

---
+++ test/ClearHl7.Fhir.Tests/SegmentsTests/Db1SegmentTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using ClearHl7.Fhir.V282.Segments;
+using ClearHl7.Fhir.V282.Types;
+using Xunit;
+
+namespace ClearHl7.Fhir.Tests.SegmentsTests
+{
+    public class Db1SegmentTests
+    {
+        /// <summary>
+        /// Validates that ToDelimitedString() returns output with all fields populated and in the correct sequence.
+        /// </summary>
+        [Fact]
+        public void ToDelimitedString_WithAllProperties_ReturnsCorrectlySequencedFields()
+        {
+            ISegment hl7Segment = new Db1Segment
+            {
+                SetIdDb1 = 1,
+                DisabledPersonCode = new CodedWithExceptions
+                {
+                    Identifier = "2"
+                },
+                DisabledPersonIdentifier = new List<ExtendedCompositeIdWithCheckDigit>
+                {
+                    new ExtendedCompositeIdWithCheckDigit
+                    {
+                        IdNumber = "3"
+                    }
+                },
+                DisabilityIndicator = "4",
+                DisabilityStartDate = new DateTime(2020, 5, 5),
+                DisabilityEndDate = new DateTime(2020, 6, 6),
+                DisabilityReturnToWorkDate = new DateTime(2020, 7, 7),
+                DisabilityUnableToWorkDate = new DateTime(2020, 8, 8)
+            };
+
+            string expected = "DB1|1|2|3|4|20200505|20200606|20200707|20200808";
+            string actual = hl7Segment.ToDelimitedString();
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ test/ClearHl7.Fhir.Tests/SegmentsTests/Dg1SegmentTests.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using ClearHl7.Fhir.V282.Segments;
+using ClearHl7.Fhir.V282.Types;
+using Xunit;
+
+namespace ClearHl7.Fhir.Tests.SegmentsTests
+{
+    public class Dg1SegmentTests
+    {
+        /// <summary>
+        /// Validates that ToDelimitedString() returns output with all fields populated and in the correct sequence.
+        /// </summary>
+        [Fact]
+        public void ToDelimitedString_WithAllProperties_ReturnsCorrectlySequencedFields()
+        {
+            ISegment hl7Segment = new Dg1Segment
+            {
+                SetIdDg1 = 1,
+                DiagnosisCodingMethod = "2",
+                DiagnosisCodeDg1 = new CodedWithExceptions
+                {
+                    Identifier = "3"
+                },
+                DiagnosisDescription = "4",
+                DiagnosisDateTime = new DateTime(2020,5,5,0,0,5),
+                DiagnosisType = new CodedWithExceptions
+                {
+                    Identifier = "6"
+                },
+                MajorDiagnosticCategory = new CodedWithNoExceptions
+                {
+                    Identifier = "7"
+                },
+                DiagnosticRelatedGroup = new CodedWithNoExceptions
+                {
+                    Identifier = "8"
+                },
+                DrgApprovalIndicator = "9",
+                DrgGrouperReviewCode = new CodedWithExceptions
+                {
+                    Identifier ="10"
+                },
+                OutlierType = new CodedWithExceptions
+                {
+                    Identifier = "11"
+                },
+                OutlierDays = 12,
+                OutlierCost = new CompositePrice
+                {
+                    Price = new Money
+                    {
+                        Quantity = 13
+                    }
+                },
+                GrouperVersionAndType = "14",
+                DiagnosisPriority = 15,
+                DiagnosingClinician = new List<ExtendedCompositeIdNumberAndNameForPersons>
+                {
+                    new ExtendedCompositeIdNumberAndNameForPersons
+                    {
+                        PersonIdentifier = "16"
+                    }
+                },
+                DiagnosisClassification = new CodedWithExceptions
+                {
+                    Identifier = "17"
+                },
+                ConfidentialIndicator = "18",
+                AttestationDateTime = new DateTime(2020,1,19,0,0,19),
+                DiagnosisIdentifier = new EntityIdentifier
+                {
+                    EntityId = "20"
+                },
+                DiagnosisActionCode = "21",
+                ParentDiagnosis = new EntityIdentifier
+                {
+                    EntityId ="22"
+                },
+                DrgCclValueCode = new CodedWithExceptions
+                {
+                    Identifier = "23"
+                },
+                DrgGroupingUsage = "24",
+                DrgDiagnosisDeterminationStatus = new CodedWithExceptions
+                {
+                    Identifier = "25"
+                },
+                PresentOnAdmissionPoaIndicator = new CodedWithExceptions
+                {
+                    Identifier = "26"
+                }
+            };
+
+            string expected = "DG1|1|2|3|4|20200505000005|6|7|8|9|10|11|12|13|14|15|16|17|18|20200119000019|20|21|22|23|24|25|26";
+            string actual = hl7Segment.ToDelimitedString();
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
