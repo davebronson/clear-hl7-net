34bb9663142617e731bc703cc7b34aa6e1cb082f
davebronson <dave.bronson@gmail.com>
2021-01-09 21:33:01 -0500

    Added CodeExpandedYesNoIndicator enum.

---
+++ src/ClearHl7.Fhir.Codes/V251/CodeExpandedYesNoIndicator.cs
@@ -0,0 +1,54 @@
+﻿namespace ClearHl7.Fhir.Codes.V251
+{
+    /// <summary>
+    /// HL7 Version 2 Table 0532 - Expanded Yes/No Indicator.
+    /// </summary>
+    /// <remarks>https://www.hl7.org/fhir/v2/0532</remarks>
+    public enum CodeExpandedYesNoIndicator
+    {
+        /// <summary>
+        /// ASKU - Asked But Unknown.
+        /// </summary>
+        AskedButUnknown,
+
+        /// <summary>
+        /// N - No.
+        /// </summary>
+        No,
+
+        /// <summary>
+        /// NA - Not Applicable.
+        /// </summary>
+        NotApplicable,
+
+        /// <summary>
+        /// NASK - Not Asked.
+        /// </summary>
+        NotAsked,
+
+        /// <summary>
+        /// NAV - Temporarily Unavailable.
+        /// </summary>
+        TemporarilyUnavailable,
+
+        /// <summary>
+        /// NI - No Information.
+        /// </summary>
+        NoInformation,
+
+        /// <summary>
+        /// NP - Not Present.
+        /// </summary>
+        NotPresent,
+
+        /// <summary>
+        /// Unk - Unknown.
+        /// </summary>
+        Unknown,
+
+        /// <summary>
+        /// Y - Yes.
+        /// </summary>
+        Yes
+    }
+}
