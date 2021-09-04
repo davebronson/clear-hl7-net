0bfcea2b309cedd3018e61c9e6ec0ce537ae031e
davebronson <dave.bronson@gmail.com>
2021-09-04 14:08:45 -0400

    Simplify MessageHelper.DetectVersion(), Add MessageHelper unit tests

--- src/ClearHl7/Helpers/MessageHelper.cs
+++ src/ClearHl7/Helpers/MessageHelper.cs
@@ -1,5 +1,5 @@
-﻿using System.Globalization;
-using System.Text;
+﻿using System;
+using System.Globalization;
 
 namespace ClearHl7.Helpers
 {
@@ -12,76 +12,100 @@ namespace ClearHl7.Helpers
         /// Attempts to detect the HL7 version, given the message's delimited string.
         /// </summary>
         /// <param name="delimitedString">A string representation of an HL7 message.</param>
-        /// <returns>An Hl7Version enuemration value.</returns>
+        /// <returns>An Hl7Version enumeration value.</returns>
         /// <remarks>A properly formed HL7 message is expected, with the MSH segment appearing first and the version appearing in the 12th delimited position.</remarks>
-        public static Hl7Version DetectHl7Version(string delimitedString)
+        /// <exception cref="ArgumentNullException">delimitedString is null.</exception>
+        /// <exception cref="ArgumentException">
+        /// delimitedString string does not begin with an MSH segment.
+        /// -or-
+        /// delimitedString is not valid.
+        /// </exception>
+        public static Hl7Version DetectVersion(string delimitedString)
         {
-            if (string.IsNullOrEmpty(delimitedString)) { return Hl7Version.None; }
+            CultureInfo culture = CultureInfo.CurrentCulture;
 
+            if (delimitedString == null) { throw new ArgumentNullException($"{ delimitedString } is null.", nameof(delimitedString)); }
+            if (delimitedString.Length < 5) { throw new ArgumentException($"{ delimitedString } is not valid.", nameof(delimitedString)); }
+            if (!delimitedString.StartsWith("MSH", true, culture)) { throw new ArgumentException($"{ delimitedString } string does not begin with an MSH segment.", nameof(delimitedString)); }
+
+            int delimitedStringEndPosition = delimitedString.Length - 1;
+            int startPosition = 0;
+            
             // The value for HL7 Version lives at position 12
             // Find the starting position of #12
-            int startPosition = 0;
             for (int i = 0; i < 11; i++)
             {
-                //if (startPosition == delimitedString.Length)
-                //{
-                //    return Hl7Version.None;
-                //}
-
                 startPosition = delimitedString.IndexOf(Consts.DefaultFieldSeparator, startPosition + 1);
 
-                if (startPosition == -1)
+                if (startPosition == -1 || startPosition == delimitedStringEndPosition)
                 {
                     return Hl7Version.None;
                 }
             }
 
-            if (startPosition > -1 && startPosition < delimitedString.Length)
+            startPosition++;
+
+            // Find the ending position of the version identifier
+            int endPosition = delimitedString.IndexOfAny(new[] { Consts.DefaultFieldSeparator[0], delimitedString[4] }, startPosition);
+
+            if (endPosition == -1)
             {
-                StringBuilder builder = new();
-
-                // Take chars until we reach 5 chars (max), or a non-numeric char, or a period "."
-                for (int i = startPosition + 1; i < startPosition + 6; i++)
-                {
-                    if (char.IsNumber(delimitedString[i]))
-                    {
-                        builder.Append(delimitedString[i]);
-                        continue;
-                    }
-                    if (delimitedString[i].Equals('.'))
-                    {
-                        builder.Append(delimitedString[i]);
-                        continue;
-                    }
-
-                    break;
-                }
-
-                string version = builder.ToString();
-                CultureInfo culture = CultureInfo.CurrentCulture;
-
-                // Map and return
-                if (string.Compare("2.3", version, false, culture) == 0) { return Hl7Version.V230; }
-                if (string.Compare("2.3.1", version, false, culture) == 0) { return Hl7Version.V231; }
-                if (string.Compare("2.4", version, false, culture) == 0) { return Hl7Version.V240; }
-                if (string.Compare("2.5", version, false, culture) == 0) { return Hl7Version.V250; }
-                if (string.Compare("2.5.1", version, false, culture) == 0) { return Hl7Version.V251; }
-                if (string.Compare("2.6", version, false, culture) == 0) { return Hl7Version.V260; }
-                if (string.Compare("2.7", version, false, culture) == 0) { return Hl7Version.V270; }
-                if (string.Compare("2.7.1", version, false, culture) == 0) { return Hl7Version.V271; }
-                if (string.Compare("2.8", version, false, culture) == 0) { return Hl7Version.V280; }
-                if (string.Compare("2.8.1", version, false, culture) == 0) { return Hl7Version.V281; }
-                if (string.Compare("2.8.2", version, false, culture) == 0) { return Hl7Version.V282; }
-                if (string.Compare("2.9", version, false, culture) == 0) { return Hl7Version.V290; }
+                endPosition = delimitedStringEndPosition + 1;
             }
+
+            // Extract the version string
+            string versionString = delimitedString.Substring(startPosition, endPosition - startPosition);
+            
+            // Map and return
+            if (string.Compare("2.3", versionString, false, culture) == 0) { return Hl7Version.V230; }
+            if (string.Compare("2.3.1", versionString, false, culture) == 0) { return Hl7Version.V231; }
+            if (string.Compare("2.4", versionString, false, culture) == 0) { return Hl7Version.V240; }
+            if (string.Compare("2.5", versionString, false, culture) == 0) { return Hl7Version.V250; }
+            if (string.Compare("2.5.1", versionString, false, culture) == 0) { return Hl7Version.V251; }
+            if (string.Compare("2.6", versionString, false, culture) == 0) { return Hl7Version.V260; }
+            if (string.Compare("2.7", versionString, false, culture) == 0) { return Hl7Version.V270; }
+            if (string.Compare("2.7.1", versionString, false, culture) == 0) { return Hl7Version.V271; }
+            if (string.Compare("2.8", versionString, false, culture) == 0) { return Hl7Version.V280; }
+            if (string.Compare("2.8.1", versionString, false, culture) == 0) { return Hl7Version.V281; }
+            if (string.Compare("2.8.2", versionString, false, culture) == 0) { return Hl7Version.V282; }
+            if (string.Compare("2.9", versionString, false, culture) == 0) { return Hl7Version.V290; }
+            // The following possibilities shouldn't pop up, technically, but we'll account for them to be helpful
+            if (string.Compare("2.3.0", versionString, false, culture) == 0) { return Hl7Version.V230; }
+            if (string.Compare("2.4.0", versionString, false, culture) == 0) { return Hl7Version.V240; }
+            if (string.Compare("2.5.0", versionString, false, culture) == 0) { return Hl7Version.V250; }
+            if (string.Compare("2.6.0", versionString, false, culture) == 0) { return Hl7Version.V260; }
+            if (string.Compare("2.7.0", versionString, false, culture) == 0) { return Hl7Version.V270; }
+            if (string.Compare("2.8.0", versionString, false, culture) == 0) { return Hl7Version.V280; }
+            if (string.Compare("2.9.0", versionString, false, culture) == 0) { return Hl7Version.V290; }
 
             // Default
             return Hl7Version.None;
         }
 
-        public static IMessage MessageForHl7Version()
+        /// <summary>
+        /// Creates a new instance of the Message class that's appropriate for the given HL7 version.
+        /// </summary>
+        /// <param name="version">The HL7 version.</param>
+        /// <returns>An instance of the Message class.</returns>
+        /// <exception cref="ArgumentException">An invalid HL7 version was provided.</exception>
+        public static IMessage NewMessageForVersion(Hl7Version version)
         {
-            return new V290.Message();
+            return version switch
+            {
+                Hl7Version.V230 => new V230.Message(),
+                Hl7Version.V231 => new V231.Message(),
+                Hl7Version.V240 => new V240.Message(),
+                Hl7Version.V250 => new V250.Message(),
+                Hl7Version.V251 => new V251.Message(),
+                Hl7Version.V260 => new V260.Message(),
+                Hl7Version.V270 => new V270.Message(),
+                Hl7Version.V271 => new V271.Message(),
+                Hl7Version.V280 => new V280.Message(),
+                Hl7Version.V281 => new V281.Message(),
+                Hl7Version.V282 => new V282.Message(),
+                Hl7Version.V290 => new V290.Message(),
+                _ => throw new ArgumentException("An invalid version was provided.", nameof(version)),
+            };
         }
     }
 }
---
+++ test/ClearHl7.Tests/HelpersTests/MessageHelperTests.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using ClearHl7.Helpers;
+using Xunit;
+
+namespace ClearHl7.Tests.HelpersTests
+{
+    public class MessageHelperTests
+    {
+        /// <summary>
+        /// Validates that DetectVersion() returns the correct HL7 version enum value when V2.3 is specified.
+        /// </summary>
+        [Fact]
+        public void DetectVersion_With23Specified_ReturnsCorrectHl7Version()
+        {
+            Assert.True(MessageHelper.DetectVersion(@"MSH|^~\&||||||||||2.3||") == Hl7Version.V230);
+        }
+
+        /// <summary>
+        /// Validates that DetectVersion() returns the correct HL7 version enum value when V2.3.0 is specified.
+        /// </summary>
+        [Fact]
+        public void DetectVersion_With230Specified_ReturnsCorrectHl7Version()
+        {
+            Assert.True(MessageHelper.DetectVersion(@"MSH|^~\&||||||||||2.3.0||") == Hl7Version.V230);
+        }
+
+        /// <summary>
+        /// Validates that DetectVersion() returns the correct HL7 version enum value when V2.3.1 is specified.
+        /// </summary>
+        [Fact]
+        public void DetectVersion_With231Specified_ReturnsCorrectHl7Version()
+        {
+            Assert.True(MessageHelper.DetectVersion(@"MSH|^~\&||||||||||2.3.1||") == Hl7Version.V231);
+        }
+
+        /// <summary>
+        /// Validates that DetectVersion() returns the correct HL7 version enum value when V2.4 is specified.
+        /// </summary>
+        [Fact]
+        public void DetectVersion_With24Specified_ReturnsCorrectHl7Version()
+        {
+            Assert.True(MessageHelper.DetectVersion(@"MSH|^~\&||||||||||2.4||") == Hl7Version.V240);
+        }
+
+        /// <summary>
+        /// Validates that DetectVersion() returns the correct HL7 version enum value when V2.4.0 is specified.
+        /// </summary>
+        [Fact]
+        public void DetectVersion_With240Specified_ReturnsCorrectHl7Version()
+        {
+            Assert.True(MessageHelper.DetectVersion(@"MSH|^~\&||||||||||2.4.0||") == Hl7Version.V240);
+        }
+
+        /// <summary>
+        /// Validates that DetectVersion() returns the correct HL7 version enum value when V2.5 is specified.
+        /// </summary>
+        [Fact]
+        public void DetectVersion_With25Specified_ReturnsCorrectHl7Version()
+        {
+            Assert.True(MessageHelper.DetectVersion(@"MSH|^~\&||||||||||2.5||") == Hl7Version.V250);
+        }
+
+        /// <summary>
+        /// Validates that DetectVersion() returns the correct HL7 version enum value when V2.5.0 is specified.
+        /// </summary>
+        [Fact]
+        public void DetectVersion_With250Specified_ReturnsCorrectHl7Version()
+        {
+            Assert.True(MessageHelper.DetectVersion(@"MSH|^~\&||||||||||2.5.0||") == Hl7Version.V250);
+        }
+
+        /// <summary>
+        /// Validates that DetectVersion() returns the correct HL7 version enum value when V2.5.1 is specified.
+        /// </summary>
+        [Fact]
+        public void DetectVersion_With251Specified_ReturnsCorrectHl7Version()
+        {
+            Assert.True(MessageHelper.DetectVersion(@"MSH|^~\&||||||||||2.5.1||") == Hl7Version.V251);
+        }
+
+        /// <summary>
+        /// Validates that DetectVersion() returns the correct HL7 version enum value when V2.6 is specified.
+        /// </summary>
+        [Fact]
+        public void DetectVersion_With26Specified_ReturnsCorrectHl7Version()
+        {
+            Assert.True(MessageHelper.DetectVersion(@"MSH|^~\&||||||||||2.6||") == Hl7Version.V260);
+        }
+
+        /// <summary>
+        /// Validates that DetectVersion() returns the correct HL7 version enum value when V2.6.0 is specified.
+        /// </summary>
+        [Fact]
+        public void DetectVersion_With260Specified_ReturnsCorrectHl7Version()
+        {
+            Assert.True(MessageHelper.DetectVersion(@"MSH|^~\&||||||||||2.6.0||") == Hl7Version.V260);
+        }
+
+        /// <summary>
+        /// Validates that DetectVersion() returns the correct HL7 version enum value when V2.7 is specified.
+        /// </summary>
+        [Fact]
+        public void DetectVersion_With27Specified_ReturnsCorrectHl7Version()
+        {
+            Assert.True(MessageHelper.DetectVersion(@"MSH|^~\&||||||||||2.7||") == Hl7Version.V270);
+        }
+
+        /// <summary>
+        /// Validates that DetectVersion() returns the correct HL7 version enum value when V2.7.0 is specified.
+        /// </summary>
+        [Fact]
+        public void DetectVersion_With270Specified_ReturnsCorrectHl7Version()
+        {
+            Assert.True(MessageHelper.DetectVersion(@"MSH|^~\&||||||||||2.7.0||") == Hl7Version.V270);
+        }
+
+        /// <summary>
+        /// Validates that DetectVersion() returns the correct HL7 version enum value when V2.7.1 is specified.
+        /// </summary>
+        [Fact]
+        public void DetectVersion_With271Specified_ReturnsCorrectHl7Version()
+        {
+            Assert.True(MessageHelper.DetectVersion(@"MSH|^~\&||||||||||2.7.1||") == Hl7Version.V271);
+        }
+
+        /// <summary>
+        /// Validates that DetectVersion() returns the correct HL7 version enum value when V2.8 is specified.
+        /// </summary>
+        [Fact]
+        public void DetectVersion_With28Specified_ReturnsCorrectHl7Version()
+        {
+            Assert.True(MessageHelper.DetectVersion(@"MSH|^~\&||||||||||2.8||") == Hl7Version.V280);
+        }
+
+        /// <summary>
+        /// Validates that DetectVersion() returns the correct HL7 version enum value when V2.8.0 is specified.
+        /// </summary>
+        [Fact]
+        public void DetectVersion_With280Specified_ReturnsCorrectHl7Version()
+        {
+            Assert.True(MessageHelper.DetectVersion(@"MSH|^~\&||||||||||2.8.0||") == Hl7Version.V280);
+        }
+
+        /// <summary>
+        /// Validates that DetectVersion() returns the correct HL7 version enum value when V2.8.1 is specified.
+        /// </summary>
+        [Fact]
+        public void DetectVersion_With281Specified_ReturnsCorrectHl7Version()
+        {
+            Assert.True(MessageHelper.DetectVersion(@"MSH|^~\&||||||||||2.8.1||") == Hl7Version.V281);
+        }
+
+        /// <summary>
+        /// Validates that DetectVersion() returns the correct HL7 version enum value when V2.8.2 is specified.
+        /// </summary>
+        [Fact]
+        public void DetectVersion_With282Specified_ReturnsCorrectHl7Version()
+        {
+            Assert.True(MessageHelper.DetectVersion(@"MSH|^~\&||||||||||2.8.2||") == Hl7Version.V282);
+        }
+
+        /// <summary>
+        /// Validates that DetectVersion() returns the correct HL7 version enum value when V2.9 is specified.
+        /// </summary>
+        [Fact]
+        public void DetectVersion_With29Specified_ReturnsCorrectHl7Version()
+        {
+            Assert.True(MessageHelper.DetectVersion(@"MSH|^~\&||||||||||2.9||") == Hl7Version.V290);
+        }
+
+        /// <summary>
+        /// Validates that DetectVersion() returns the correct HL7 version enum value when V2.9.0 is specified.
+        /// </summary>
+        [Fact]
+        public void DetectVersion_With290Specified_ReturnsCorrectHl7Version()
+        {
+            Assert.True(MessageHelper.DetectVersion(@"MSH|^~\&||||||||||2.9.0||") == Hl7Version.V290);
+        }
+
+        /// <summary>
+        /// Validates that DetectVersion() returns the correct HL7 version enum value when position 12 includes multiple components.
+        /// </summary>
+        [Fact]
+        public void DetectVersion_WithMultipleComponentsInPositionTwelve_ReturnsCorrectHl7Version()
+        {
+            Assert.True(MessageHelper.DetectVersion(@"MSH|^~\&||||||||||2.5.1^something else||") == Hl7Version.V251);
+        }
+
+        /// <summary>
+        /// Validates that DetectVersion() returns the correct HL7 version enum value when a custom component separator is used.
+        /// </summary>
+        [Fact]
+        public void DetectVersion_WithCustomComponentSeparator_ReturnsCorrectHl7Version()
+        {
+            Assert.True(MessageHelper.DetectVersion(@"MSH|$~\&||||||||||2.5.1$something else||") == Hl7Version.V251);
+        }
+
+        /// <summary>
+        /// Validates that DetectVersion() returns the correct HL7 version enum value when exactly 12 positions are provided.
+        /// </summary>
+        [Fact]
+        public void DetectVersion_WithExactly12Positions_ReturnsCorrectHl7Version()
+        {
+            Assert.True(MessageHelper.DetectVersion(@"MSH|^~\&||||||||||2.5.1") == Hl7Version.V251);
+        }
+
+        /// <summary>
+        /// Validates that DetectVersion() returns the correct HL7 version enum value when less than 12 positions are provided.
+        /// </summary>
+        [Fact]
+        public void DetectVersion_WithLessThan12Positions_ReturnsCorrectHl7Version()
+        {
+            Assert.True(MessageHelper.DetectVersion(@"MSH|^~\&||||||") == Hl7Version.None);
+        }
+
+        /// <summary>
+        /// Validates that DetectVersion() throws an ArgumentNullException when a null delimitedString is provided.
+        /// </summary>
+        [Fact]
+        public void DetectVersion_WithNullInput_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                MessageHelper.DetectVersion(null);
+            });
+        }
+
+        /// <summary>
+        /// Validates that DetectVersion() throws an ArgumentException when an under-length delimitedString is provided.
+        /// </summary>
+        [Fact]
+        public void DetectVersion_WithLessThan5CharInput_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                MessageHelper.DetectVersion(@"MSH|");
+            });
+        }
+
+        /// <summary>
+        /// Validates that DetectVersion() throws an ArgumentException when a delimitedString not beginning with an MSH segment is provided.
+        /// </summary>
+        [Fact]
+        public void DetectVersion_WithInvalidFirstSegment_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                MessageHelper.DetectVersion(@"ABC|^~\&||||||||||2.8.0||");
+            });
+        }
+
+        /// <summary>
+        /// Validates that NewMessageForVersion() returns the correct message instance when V2.3 is specified.
+        /// </summary>
+        [Fact]
+        public void NewMessageForVersion_With23Specified_ReturnsCorrectHl7Version()
+        {
+            Assert.IsType<V230.Message>(MessageHelper.NewMessageForVersion(Hl7Version.V230));
+        }
+
+        /// <summary>
+        /// Validates that NewMessageForVersion() returns the correct message instance when V2.3.1 is specified.
+        /// </summary>
+        [Fact]
+        public void NewMessageForVersion_Wit231Specified_ReturnsCorrectHl7Version()
+        {
+            Assert.IsType<V231.Message>(MessageHelper.NewMessageForVersion(Hl7Version.V231));
+        }
+
+        /// <summary>
+        /// Validates that NewMessageForVersion() returns the correct message instance when V2.4 is specified.
+        /// </summary>
+        [Fact]
+        public void NewMessageForVersion_With24Specified_ReturnsCorrectHl7Version()
+        {
+            Assert.IsType<V240.Message>(MessageHelper.NewMessageForVersion(Hl7Version.V240));
+        }
+
+        /// <summary>
+        /// Validates that NewMessageForVersion() returns the correct message instance when V2.5 is specified.
+        /// </summary>
+        [Fact]
+        public void NewMessageForVersion_With25Specified_ReturnsCorrectHl7Version()
+        {
+            Assert.IsType<V250.Message>(MessageHelper.NewMessageForVersion(Hl7Version.V250));
+        }
+
+        /// <summary>
+        /// Validates that NewMessageForVersion() returns the correct message instance when V2.5.1 is specified.
+        /// </summary>
+        [Fact]
+        public void NewMessageForVersion_With251Specified_ReturnsCorrectHl7Version()
+        {
+            Assert.IsType<V251.Message>(MessageHelper.NewMessageForVersion(Hl7Version.V251));
+        }
+
+        /// <summary>
+        /// Validates that NewMessageForVersion() returns the correct message instance when V2.6 is specified.
+        /// </summary>
+        [Fact]
+        public void NewMessageForVersion_With26Specified_ReturnsCorrectHl7Version()
+        {
+            Assert.IsType<V260.Message>(MessageHelper.NewMessageForVersion(Hl7Version.V260));
+        }
+
+        /// <summary>
+        /// Validates that NewMessageForVersion() returns the correct message instance when V2.7 is specified.
+        /// </summary>
+        [Fact]
+        public void NewMessageForVersion_With27Specified_ReturnsCorrectHl7Version()
+        {
+            Assert.IsType<V270.Message>(MessageHelper.NewMessageForVersion(Hl7Version.V270));
+        }
+
+        /// <summary>
+        /// Validates that NewMessageForVersion() returns the correct message instance when V2.7.1 is specified.
+        /// </summary>
+        [Fact]
+        public void NewMessageForVersion_With271Specified_ReturnsCorrectHl7Version()
+        {
+            Assert.IsType<V271.Message>(MessageHelper.NewMessageForVersion(Hl7Version.V271));
+        }
+
+        /// <summary>
+        /// Validates that NewMessageForVersion() returns the correct message instance when V2.8 is specified.
+        /// </summary>
+        [Fact]
+        public void NewMessageForVersion_With28Specified_ReturnsCorrectHl7Version()
+        {
+            Assert.IsType<V280.Message>(MessageHelper.NewMessageForVersion(Hl7Version.V280));
+        }
+
+        /// <summary>
+        /// Validates that NewMessageForVersion() returns the correct message instance when V2.8.1 is specified.
+        /// </summary>
+        [Fact]
+        public void NewMessageForVersion_With281Specified_ReturnsCorrectHl7Version()
+        {
+            Assert.IsType<V281.Message>(MessageHelper.NewMessageForVersion(Hl7Version.V281));
+        }
+
+        /// <summary>
+        /// Validates that NewMessageForVersion() returns the correct message instance when V2.8.2 is specified.
+        /// </summary>
+        [Fact]
+        public void NewMessageForVersion_With282Specified_ReturnsCorrectHl7Version()
+        {
+            Assert.IsType<V282.Message>(MessageHelper.NewMessageForVersion(Hl7Version.V282));
+        }
+
+        /// <summary>
+        /// Validates that NewMessageForVersion() returns the correct message instance when V2.9 is specified.
+        /// </summary>
+        [Fact]
+        public void NewMessageForVersion_With29Specified_ReturnsCorrectHl7Version()
+        {
+            Assert.IsType<V290.Message>(MessageHelper.NewMessageForVersion(Hl7Version.V290));
+        }
+    }
+}
